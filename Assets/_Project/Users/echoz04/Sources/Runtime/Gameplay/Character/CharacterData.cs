a303980636febd198d148b00db42ec3edcd57bbb
cindesebos <y07programmer@gmail.com>
2025-10-28 19:47:30 +0100

    Улучшил персонажа

--- Assets/_Project/Users/echoz04/Sources/Runtime/Gameplay/Character/CharacterData.cs
+++ Assets/_Project/Users/echoz04/Sources/Runtime/Gameplay/Character/CharacterData.cs
@@ -9,5 +9,7 @@ namespace Sources.Runtime.Gameplay.Character
         [field: SerializeField] public float JumpForce { get; private set; } = 5f;
         [field: SerializeField] public float GroundStickForce { get; private set; } = -2f;
         [field: SerializeField] public float Gravity { get; private set; } = -9.81f;
+        [field: SerializeField] public float CoyoteTime { get; private set; } = 0.15f;
+        [field: SerializeField] public float JumpBufferTime { get; private set; } = 0.1f;
     }
 }
--- Assets/_Project/Users/echoz04/Sources/Runtime/Gameplay/Character/CharacterJumper.cs
+++ Assets/_Project/Users/echoz04/Sources/Runtime/Gameplay/Character/CharacterJumper.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Sources.Runtime.Gameplay.Character
@@ -12,6 +14,9 @@ namespace Sources.Runtime.Gameplay.Character
         private readonly CharacterInput _input;
 
         private float _verticalVelocity;
+        private float _coyoteTimeCounter;
+        private bool _jumpBuffered;
+        private CancellationTokenSource _jumpBufferCts;
 
         public CharacterJumper(CharacterController controller, CharacterData data, CharacterInput input)
         {
@@ -19,30 +24,72 @@ namespace Sources.Runtime.Gameplay.Character
             _data = data;
             _input = input;
 
-            _input.Movement.Jump.performed += context => Jump();
+            _input.Movement.Jump.performed += OnJumpPressed;
         }
 
         public void Tick()
         {
             bool isGrounded = _controller.isGrounded;
 
-            if (isGrounded == true && _verticalVelocity < 0f)
+            if (isGrounded)
+                _coyoteTimeCounter = _data.CoyoteTime;
+            else
+                _coyoteTimeCounter -= Time.deltaTime;
+
+            if (_jumpBuffered && _coyoteTimeCounter > 0f)
+            {
+                PerformJump();
+                _jumpBuffered = false;
+                _jumpBufferCts?.Cancel();
+            }
+
+            if (isGrounded && _verticalVelocity < 0f)
                 _verticalVelocity = _data.GroundStickForce;
 
             _verticalVelocity += _data.Gravity * Time.deltaTime;
 
             Vector3 verticalMove = new Vector3(0f, _verticalVelocity, 0f) * Time.deltaTime;
-            
             _controller.Move(verticalMove);
         }
 
-        private void Jump()
+        private void OnJumpPressed(UnityEngine.InputSystem.InputAction.CallbackContext context)
         {
-            if (IsGrounded == true)
-                _verticalVelocity = Mathf.Sqrt(_data.JumpForce * -2f * _data.Gravity);
+            if (_coyoteTimeCounter > 0f)
+            {
+                PerformJump();
+            }
+            else
+            {
+                _jumpBuffered = true;
+                _jumpBufferCts?.Cancel();
+                _jumpBufferCts = new CancellationTokenSource();
+                WaitAndClearJumpBufferAsync(_jumpBufferCts.Token).Forget();
+            }
         }
-        
-        public void Dispose() => 
-            _input.Movement.Jump.performed -= context => Jump();
+
+        private void PerformJump()
+        {
+            _verticalVelocity = Mathf.Sqrt(_data.JumpForce * -2f * _data.Gravity);
+            _coyoteTimeCounter = 0f;
+        }
+
+        private async UniTaskVoid WaitAndClearJumpBufferAsync(CancellationToken token)
+        {
+            try
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(_data.JumpBufferTime), cancellationToken: token);
+                _jumpBuffered = false;
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            _input.Movement.Jump.performed -= OnJumpPressed;
+            _jumpBufferCts?.Cancel();
+            _jumpBufferCts?.Dispose();
+        }
     }
 }
